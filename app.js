766903acaba848c5e307c764801c291a4396d693
Muhammad Saad <syedmuhammadsaad@live.com>
2020-12-03 23:03:22 +0500

    added and configured ejs, created a new file profile.ejs to test the results between backend and frontend

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var express = require('express');
 // setting up express
 var app = express();
 
+// setting up ejs
+app.set('view engine','ejs');
+
 // default route
 app.get('/',(req, res) => {
     res.sendFile(__dirname + '/client/index.html');
@@ -15,9 +18,15 @@ app.get('/contact',(req, res) => {
     res.sendFile(__dirname + '/client/contact.html');
 });
 
-// profile route with params
-app.get('/profile/:name',(req, res) => {
+// prof route with params
+app.get('/prof/:name',(req, res) => {
     res.send(`Welcome to profile of ${req.params.name}`);
+});
+
+// profile route with ejs and params
+app.get('/profile/:name', (req, res) => {
+    var data = {age: 31, job: 'Sr. Full Stack Developer', hobbies: ['Gaming','Programming','Animes']};
+    res.render('profile', {name: req.params.name, data: data});
 });
 
 // 404 route
